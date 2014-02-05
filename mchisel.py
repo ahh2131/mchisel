731e2a9c8cdce214590c06f88328eff1642f7bf7
Andy <ahh2131@columbia.edu>
2014-02-04 22:54:04 -0500

    Added clean code and updated main function

--- mchisel.py
+++ mchisel.py
@@ -1,17 +1,19 @@
-# mchisel
+# mchizzle
 from subprocess import call
 import json
 from pprint import pprint
-
+import sys
 # arguments - 3 json files (op), map name (opt), player name (opt)
 def main(*args):
     # creates variables depending on if argument is available
-    one = args[0] if args[0] else "json/one.json"
-    two = args[1] if args[1] else "json/two.json"
-    three = args[2] if args[2] else "json/three.json"
-    mapName = args[3] if args[3] else "map/level.dat"
-    playerName = args[4] if args[4] else "Player"
-
+    one = args[1] if (len(args) > 1) else "json/one.json"
+    """"
+    two = args[2] if (len(args) > 2) else "json/two.json"
+    three = args[3] if (len(args) > 3) else "json/three.json"
+    """
+    mapName = args[4] if (len(args) > 4) else "minecraft/flatland"
+    playerName = args[5] if (len(args) > 5) else "Player"
+    """
     json_one = open(one)
     one_data = json.load(json_one)
     json_one.close()
@@ -23,13 +25,24 @@ def main(*args):
     json_two = open(three)
     json_data = json.load(json_three)
     json_three.close()
-  	    
-    runTool(mapName, playerName)
+   """	    
+    runTool(mapName) 
+    createObject(one, playerName) 
+    exportMap()
+    cleanMap()
+
+def createObject(data, playerName):
     
 
 # runs command line minecraft editor in shell
-def runTool(mapName, playerName):    
+def runTool(mapName):    
+    call(["python", "mce.py", mapName])
 
-main(*args)
+# cleans map 
+def cleanMap():
+    call(["clean"])
+
+
+main(sys.argv)
 
 
